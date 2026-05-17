97e2af38a406277a82282a741ed2d8e3a368589c
Zacharia316 <zappyblues234@gmail.com>
2026-05-17 23:27:36 +0300

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import HiveApp from './hive.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HiveApp />
+  </React.StrictMode>
+)
